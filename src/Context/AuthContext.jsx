4ba821626515ffa5126575c50ff1ffd31b70dae6
uthran <uthiranuthiran22@gmail.com>
2026-04-10 12:13:18 +0530

    url for Netlify

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
-import { API_BASE_URL } from "../App";
+import { API_BASE_URL } from "../Services/apiservice"
 import { Road } from "lucide-react";
 
 export const AuthContext=createContext();
